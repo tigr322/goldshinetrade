2b93669ea372847ed4f6372e82ac00aa72946d2f
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-18 01:22:25 +0300

    Перенос всех файлов с сервера

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
+    /**в
      * The attributes that are mass assignable.
      *
      * @var list<string>
