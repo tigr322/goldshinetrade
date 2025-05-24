d08586e3a90e5ec076bccb42fee15816dc90d750
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-24 14:03:07 +0300

    Валидализацию кошелька

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+<!-- Внутри <head> -->
+    <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
