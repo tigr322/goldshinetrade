1bd8bf2f0482bf2bddfcd59eb13ab095e806e5de
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-24 19:47:00 +0300

    Админ панель

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-<!-- Внутри вывы<head> в-->
+<!-- Внутриd вывы<head> в-->
     <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
