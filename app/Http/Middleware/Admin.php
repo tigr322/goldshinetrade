37e8eafac1aff06fa32b68e433ecdfc759b27b29
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-05-25 20:52:18 +0300

    Админ панель

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -5,7 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-//Проверка на админаd
+//Проверка на админаdв
+
 class Admin
 {
     /**
