c917451df09607173593603e01708bcddd61f0a8
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-06-15 22:07:35 +0300

    Добавил график

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -105,8 +105,8 @@ function handleImageError() {
               </div>
             </div>
             <div class="text-center">
-              <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">GoldShineTrade — обменяй игровые достижения на реальные возможности!</h1>
-              <p class="mt-6 text-lg leading-8 text-gray-600">Продавайте и получайте реальные денежные средства в течение всего дня, без необходимости выхода из дома!</p>
+             <!-- <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">GoldShineTrade — обменяй игровые достижения на реальные возможности!</h1>
+              <p class="mt-6 text-lg leading-8 text-gray-600">Продавайте и получайте реальные денежные средства в течение всего дня, без необходимости выхода из дома!</p>-->
              
               <div class="mt-10 flex items-center justify-center gap-x-6">
                
