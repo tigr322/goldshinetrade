b0bc1abf21cb1d806daa69700375c3330d17b29e
Тигран Адамян <tigranadaman@MacBook-Air-Tigran.local>
2025-07-12 17:59:58 +0300

    Интеграция API пополнения кошелька

--- config/ckassa.php
+++ config/ckassa.php
@@ -3,5 +3,5 @@ return [
     'token' => env('CKASSA_SHOP_TOKEN'),
     'secret' => env('CKASSA_SEC_KEY'),
     'code' => env('CKASSA_SHOP_CODE'),
-    'url' => env('CKASSA_API_URL'),
+    'url'=> env('CKASSA_URL', 'https://api2.ckassa.ru/api-shop/rs/open/invoice/create2'),
 ];
