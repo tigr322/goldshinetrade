bbf9ecfbde044aac8a0123341808a4861f773915
Tigran001 <tigran05012002@mail.ru>
2025-08-05 14:45:51 +0300

    Окончательно исключаем node_modules из Git

--- app/Http/Controllers/WalletTopupController.php
+++ app/Http/Controllers/WalletTopupController.php
@@ -38,32 +38,37 @@ public function store(Request $request)
 }
 public function handleCallback(Request $request)
 {
-    
     Log::info('CKassa Callback Received', $request->all());
     $externalId = data_get($request->input('property'), 'ЛИЦЕВОЙ_СЧЕТ');
-
+    $regPayNum = data_get($request->input('regPayNum'));
     if (!$externalId) {
         return response()->json(['error' => 'Missing external ID'], 400);
     }
+    if (!$regPayNum) {
+        return response()->json(['error' => 'Missing regPayNum'], 400);
+    }
 
+    // 5. Запрос на FZ_CHECK в CKassa
+    $checkResponse = Http::withHeaders([
+        'ApiLoginAuthorization' => config('ckassa.shop_token'),
+        'ApiAuthorization' => config('ckassa.secret_key'),
+        'Content-Type' => 'application/json',
+    ])->post('https://api2.ckassa.ru/api-shop/rs/open/invoice/verify', [
+        'regPayNum' => $request->input('regPayNum'),
+    ]);
+    
+    $checkData = $checkResponse->json();
+    
+    if (!$checkResponse->successful() || !isset($checkData['state']) || $checkData['state'] !== 'PAYED') {
+        return response()->json(['error' => 'Payment not confirmed by CKassa'], 400);
+    }
+    
     $payment = \App\Models\Payment::where('external_id', $externalId)->first();
 
     if (!$payment) {
         return response()->json(['error' => 'Payment not found'], 404);
     }
-    $checkResponse = Http::withHeaders([
-        'ApiLoginAuthorization' => config('ckassa.shop_token'),
-        'ApiAuthorization' => config('ckassa.secret_key'),
-        'Content-Type' => 'application/json',
-    ])->post('https://api2.ckassa.ru/api-shop/rs/open/invoice/fz_check', [
-        'regPayNum' => $request->input('regPayNum'),
-    ]);
 
-    $checkData = $checkResponse->json();
-
-    if (!$checkResponse->successful() || !isset($checkData['state']) || $checkData['state'] !== 'PAYED') {
-        return response()->json(['error' => 'Payment not confirmed by CKassa'], 400);
-    }
     $state = $request->input('state');
     $code = data_get($request->input('result'), 'code');
     $amount = (float) $request->input('amount') / 100;
