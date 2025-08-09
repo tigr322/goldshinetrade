6522c867a9aef2cf3857d967428587235493fe9a
Tigran001 <tigran05012002@mail.ru>
2025-08-09 12:08:45 +0300

    Добавление доп входов

--- database/migrations/2025_08_09_085729_oauth.php
+++ database/migrations/2025_08_09_085729_oauth.php
@@ -4,16 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
-return new class {
-    public function up() {
-        Schema::table('users', function (Blueprint $t) {
-            $t->json('oauth')->nullable();
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // если хочешь — поставь поле после remember_token
+            $table->json('oauth')->nullable()->after('remember_token');
+            // Если JSON не поддерживается БД, используй:
+            // $table->longText('oauth')->nullable()->after('remember_token');
         });
     }
-    public function down() {
-        Schema::table('users', function (Blueprint $t) {
-            $t->dropColumn('oauth');
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('oauth');
         });
     }
 };
