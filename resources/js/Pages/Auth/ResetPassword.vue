bbe0a12d8d3254e25b4941676884d1e8d863744d
Tigran001 <tigran05012002@mail.ru>
2025-08-10 13:32:53 +0300

    Русификация

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -82,7 +82,7 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
+                    Сменить пароль
                 </PrimaryButton>
             </div>
         </form>
