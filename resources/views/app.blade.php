905aba8c7b67e180d67851ea13f9d5a6637e9130
Tigran001 <tigran05012002@mail.ru>
2025-08-10 14:14:12 +0300

    Google tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,13 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <!-- Fonts -->
+                <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M42D7X85');</script>
+        <!-- End Google Tag Manager -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Scripts -->
@@ -28,7 +35,10 @@
             ym(103655448, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
         </script>
         <noscript><div><img src="https://mc.yandex.ru/watch/103655448" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M42D7X85"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     </body>
     
 </html>
