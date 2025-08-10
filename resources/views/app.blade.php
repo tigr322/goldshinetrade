ad19e69b8973a361a99ee2887878fbbaaccbd0a4
Tigran001 <tigran05012002@mail.ru>
2025-08-10 14:00:13 +0300

    Метрика

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,5 +16,19 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103655448', 'ym');
+
+            ym(103655448, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/103655448" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        
     </body>
+    
 </html>
